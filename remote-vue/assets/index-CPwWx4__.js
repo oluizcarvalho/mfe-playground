bc62bd92974468bc6589e929fcdfc6a7a99c84fb
oluizcarvalho <oluizcarvalho@users.noreply.github.com>
2026-03-29 04:08:15 +0000

    deploy: d5dc6ec642cc62ffcf1ecad6e8fc8dbc1fbc0aa9

--- remote-vue/assets/index-CPwWx4__.js
+++
@@ -1,5 +0,0 @@
-import { importShared } from './__federation_fn_import-Di0Fu3Ot.js';
-import App from './__federation_expose_App-C7HzA2jY.js';
-
-const {createApp} = await importShared('vue');
-createApp(App).mount("#app");
